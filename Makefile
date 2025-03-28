dcfeaea708e17a8e4d7d848be12ed0a46c9cd80c
shevaathalla <shevaathalla@gmail.com>
2025-03-28 22:04:51 +0700

    add html reporting

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ run:
 	- k6 run main.js -e HOST_URL=$(HOST_URL)
 
 run-output:
-	- k6 run main.js -e HOST_URL=$(HOST_URL) --out json=reports/results-$(shell date +%Y%m%d%H%M%S).json
+	- k6 run main.js -e HOST_URL=$(HOST_URL) --out json=reports/json/results-$(shell date +%Y%m%d%H%M%S).json
--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 import { accessHome } from "./scenarios/home.js";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { generateTimestamp } from "./utils/helper.js";
 
 export const options = {
   vus: 10,
-  duration: '10s',
+  duration: '5s',
 };
 
 export default function() {
   accessHome();
+}
+
+export function handleSummary(data) {
+  const timestamp = generateTimestamp(); 
+  const filename = `reports/html/summary-${timestamp}.html`;
+
+  return {
+    [filename]: htmlReport(data), 
+  };
 }
